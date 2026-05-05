bbeae09a8cdcf45ddf36750e198febc160d93d31
theMidnightPoet <adrian.arenas@uao.edu.co>
2026-05-04 21:56:45 -0500

    se modifica el archivo del proyecto pygame

--- pygame/prueba1.py
+++ pygame/prueba1.py
@@ -1,7 +1,16 @@
 import pygame, sys
 pygame.init()
 
-size = (800, 500)
+size = (500, 700)
+#definir colores
+#primero es rojo el segundo es verde y el tercero es azul
+BLACK = ( 0, 0, 0)
+WHITE = ( 255, 255, 255)
+GREEN = ( 0, 255, 0)
+RED = ( 255, 0, 0)
+BLUE = (0, 0, 128)
+TEAL = (0, 128, 128)
+VERDEPASTO = (0, )
 
 #CREAR VENTANA 
 screen = pygame.display.set_mode(size)
@@ -9,5 +18,17 @@ screen = pygame.display.set_mode(size)
 #creacion bucle principal
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.quit:
+        if event.type == pygame.QUIT:
             sys.exit() 
+#color del fondo
+    screen.fill(VERDEPASTO)
+#----Zona de dibujo 
+    pygame.draw.line(screen, BLUE, [0,100], [100,100], 10)
+    pygame.draw.rect(screen, TEAL, (165, 320, 170, 50))
+
+
+
+
+#Zona de dibujo----
+    #actualizar pantalla
+    pygame.display.flip()
